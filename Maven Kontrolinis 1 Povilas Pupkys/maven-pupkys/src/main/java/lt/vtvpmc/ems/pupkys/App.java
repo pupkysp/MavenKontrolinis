bad37248723294645abde5018b1385265e4ddfd4
povilas.pupkys <povilas.pupkys@gmail.com>
2017-03-14 09:19:53 +0200

    Maven kontrolinis

---
+++ Maven Kontrolinis 1 Povilas Pupkys/maven-pupkys/src/main/java/lt/vtvpmc/ems/pupkys/App.java
@@ -0,0 +1,13 @@
+package lt.vtvpmc.ems.pupkys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
